1f62654616a55aacc64cc873374b1dc774c77957
Max <maximesin21@gmail.com>
2023-11-27 14:57:00 +0700

    Restructure code using OOP and SOLID

--- src/game.ts
+++ src/game.ts
@@ -1,87 +1,44 @@
 import * as PIXI from "pixi.js";
+import { Player } from "./player";
 
-// Create PIXI application
-const app = new PIXI.Application({
-  width: 1200,
-  height: 600,
-  backgroundColor: 0x000000,
-});
-document.body.appendChild(app.view as unknown as Node);
+// Game class
+class Game {
+  private app: PIXI.Application;
+  private player: Player;
 
-// Load the knight texture
-const knightTexture = PIXI.Texture.from("images/knight.png");
+  constructor() {
+    // Create PIXI application
+    this.app = new PIXI.Application({
+      width: 1200,
+      height: 600,
+      backgroundColor: 0x000000,
+    });
+    document.body.appendChild(this.app.view as unknown as Node);
 
-// Create a player character using the knight texture
-const player = new PIXI.Sprite(knightTexture);
-const playerWidth = 100;
-const playerHeight = 100;
-player.width = playerWidth;
-player.height = playerHeight;
+    // Init player
+    this.player = new Player(this.app);
+    this.setupInput();
+    this.setupGameLoop();
+  }
 
-player.anchor.set(0.5); // Center the anchor point
-player.x = 60;
-player.y = 550;
-app.stage.addChild(player);
+  // Add event listeners
+  private setupInput(): void {
+    window.addEventListener("keydown", this.handleKeyDown.bind(this));
+    window.addEventListener("keyup", this.handleKeyUp.bind(this));
+  }
 
-// Set the speed of player movement
-const playerSpeed = 5;
+  private handleKeyDown(event: KeyboardEvent): void {
+    this.player.handleKeyDown(event);
+  }
 
-// Define the type for the keys object
-interface Keys {
-  [key: string]: boolean;
-}
+  private handleKeyUp(event: KeyboardEvent): void {
+    this.player.handleKeyUp(event);
+  }
 
-// Keyboard input handling
-const keys: Keys = {
-  w: false,
-  a: false,
-  s: false,
-  d: false,
-};
-
-// Event listeners for keydown and keyup events
-document.addEventListener("keydown", handleKeyDown);
-document.addEventListener("keyup", handleKeyUp);
-
-// Handle keydown event
-function handleKeyDown(event: KeyboardEvent) {
-  if (event.key in keys) {
-    keys[event.key] = true;
+  private setupGameLoop(): void {
+    this.app.ticker.add(() => {
+      this.player.update();
+    });
   }
 }
-
-// Handle keyup event
-function handleKeyUp(event: KeyboardEvent) {
-  if (event.key in keys) {
-    keys[event.key] = false;
-  }
-}
-
-// Game loop
-app.ticker.add(() => {
-  // Update player position based on keyboard input
-  if (keys.w) {
-    if (player.y < app.screen.height - 550) {
-      return;
-    }
-    player.y -= playerSpeed;
-  }
-  if (keys.a) {
-    if (player.x < 50) {
-      return;
-    }
-    player.x -= playerSpeed;
-  }
-  if (keys.s) {
-    if (player.y > app.screen.height - 50) {
-      return;
-    }
-    player.y += playerSpeed;
-  }
-  if (keys.d) {
-    if (player.x > app.screen.width - 55) {
-      return;
-    }
-    player.x += playerSpeed;
-  }
-});
+const game = new Game();
---
+++ src/player.ts
@@ -0,0 +1,71 @@
+import * as PIXI from "pixi.js";
+import { Keys } from "./interfaces/keys";
+
+// Player class
+export class Player {
+  private sprite: PIXI.Sprite;
+  private keys: Keys;
+  private playerSpeed: number;
+  private playerWidth: number = 100;
+  private playerHeight: number = 100;
+  private app: PIXI.Application;
+
+  constructor(app: PIXI.Application) {
+    this.app = app;
+    this.sprite = new PIXI.Sprite(PIXI.Texture.from("images/knight.png"));
+    this.sprite.width = this.playerWidth;
+    this.sprite.height = this.playerHeight;
+    this.sprite.anchor.set(0.5);
+    this.sprite.x = 60;
+    this.sprite.y = 550;
+    app.stage.addChild(this.sprite);
+
+    this.keys = {
+      w: false,
+      a: false,
+      s: false,
+      d: false,
+    };
+
+    this.playerSpeed = 5;
+  }
+
+  public handleKeyDown(event: KeyboardEvent): void {
+    if (event.key in this.keys) {
+      this.keys[event.key] = true;
+    }
+  }
+
+  public handleKeyUp(event: KeyboardEvent): void {
+    if (event.key in this.keys) {
+      this.keys[event.key] = false;
+    }
+  }
+
+  public update(): void {
+    if (this.keys.w) {
+      if (this.sprite.y < this.app.screen.height - 550) {
+        return;
+      }
+      this.sprite.y -= this.playerSpeed;
+    }
+    if (this.keys.a) {
+      if (this.sprite.x < 50) {
+        return;
+      }
+      this.sprite.x -= this.playerSpeed;
+    }
+    if (this.keys.s) {
+      if (this.sprite.y > this.app.screen.height - 50) {
+        return;
+      }
+      this.sprite.y += this.playerSpeed;
+    }
+    if (this.keys.d) {
+      if (this.sprite.x > this.app.screen.width - 55) {
+        return;
+      }
+      this.sprite.x += this.playerSpeed;
+    }
+  }
+}
