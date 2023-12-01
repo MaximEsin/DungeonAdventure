30588ec56740ee3be9862c57c73123ca22d05481
Max <maximesin21@gmail.com>
2023-12-01 13:27:06 +0700

    Add stylish background to screen

--- src/UI/interfaceManager.ts
+++ src/UI/interfaceManager.ts
@@ -1,16 +1,12 @@
 import { PlayerStats } from "../interfaces/playerStats";
 
 export class InterfaceManager {
-  private playerNameElement: HTMLParagraphElement;
   private healthElement: HTMLParagraphElement;
   private damageElement: HTMLParagraphElement;
   private armorElement: HTMLParagraphElement;
 
   constructor() {
     // Get the interface elements
-    this.playerNameElement = document.getElementById(
-      "name"
-    ) as HTMLParagraphElement;
     this.healthElement = document.getElementById(
       "health"
     ) as HTMLParagraphElement;
@@ -23,7 +19,6 @@ export class InterfaceManager {
   }
 
   public updateInterface(playerStats: PlayerStats): void {
-    this.playerNameElement.textContent = "Max";
     this.healthElement.textContent = `Health: ${playerStats.health}`;
     this.damageElement.textContent = `Damage: ${playerStats.damage}`;
     this.armorElement.textContent = `Armor: ${playerStats.armor}`;
